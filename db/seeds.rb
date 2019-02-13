45054203e9b8f0924aa43a67daba94790b6973db
Alice Brewer <alicebrewer09@gmail.com>
2019-02-13 16:40:16 +0100

    yelp

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,41 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Dishoom',
+    address:      '7 Boundary St, London E2 7JE',
+    phone_number:  '00 33 57 58 38 17',
+    category:      "french"
+  },
+  {
+    name:         'Pizza East',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    phone_number:  '00 33 57 58 38 17',
+    category:      'italian'
+  },
+   {
+    name:         'Pizza West',
+    address:      '5theuyewfg',
+    phone_number:  '00 33 57 58 38 17',
+    category:      'belgian'
+  },
+   {
+    name:         'Random resto 1',
+    address:      'sdlfj',
+    phone_number:  '00 33 57 58 38 17',
+    category:      'chinese'
+  },
+   {
+    name:         'Random resto 2',
+    address:      'dfgdfg',
+    phone_number:  '00 33 57 58 38 17',
+    category:      'japanese'
+  },
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
---
+++ test/controllers/reviews_controller_test.rb
@@ -0,0 +1,29 @@
+require 'test_helper'
+
+class ReviewsControllerTest < ActionDispatch::IntegrationTest
+  test "should get add" do
+    get reviews_add_url
+    assert_response :success
+  end
+
+  test "should get create" do
+    get reviews_create_url
+    assert_response :success
+  end
+
+  test "should get update" do
+    get reviews_update_url
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get reviews_edit_url
+    assert_response :success
+  end
+
+  test "should get new" do
+    get reviews_new_url
+    assert_response :success
+  end
+
+end
